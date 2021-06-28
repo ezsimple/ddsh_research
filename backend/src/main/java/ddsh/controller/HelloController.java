2786ee1eb55a11fb608b6cf01511eccaf4661a27
mhlee <mhlee@ezfarm.co.kr>
2021-06-28 11:30:48 +0900

    add backend

---
+++ backend/src/main/java/ddsh/controller/HelloController.java
@@ -0,0 +1,30 @@
+package ddsh.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
+import ddsh.service.HelloService;
+
+@Slf4j
+@Controller
+public class HelloController {
+	
+	@Autowired
+	HelloService helloService;
+	
+	@ResponseBody
+	@GetMapping(value = {"/hello"}, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Object hello() throws Exception {
+		return helloService.hello();
+	}
+
+	@GetMapping(value = {"/", "/error"})
+	public String index() throws Exception {
+		return "index.html";
+	}
+
+}
